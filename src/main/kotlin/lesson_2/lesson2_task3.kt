2bee9e62f5e2802eeb8051b32284e264fd1071d4
kirill <mrcreepipc@mail.ru>
2025-08-29 14:18:17 +0400

    KS-2-3 time of landing

---
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -0,0 +1,10 @@
+package org.example.lesson_2
+
+fun main() {
+    val year = "1961"
+    var hour = "09"
+    var minute = "07"
+    hour = "10"
+    minute = "55"
+    println("Время посадики $hour:$minute")
+}
