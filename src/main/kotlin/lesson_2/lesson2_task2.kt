77a8fbb51631e6158c84e11bf583ff3ebcc73911
kirill <mrcreepipc@mail.ru>
2025-08-29 14:07:07 +0400

    KS-2-2 staff and print

---
+++ src/main/kotlin/lesson_2/lesson2_task2.kt
@@ -0,0 +1,10 @@
+package org.example.lesson_2
+
+fun main() {
+    val greetingForOrder = "Спасибо за ваш заказ!"
+    var numberOfOrders = 75
+    println(greetingForOrder)
+    println(numberOfOrders)
+    //val numberOfStaff = 2000
+    var numberOfStaff = 1999
+}
