3eec100712c3e51db6c4527f397c28c34a463f43
kirill <mrcreepipc@mail.ru>
2025-08-30 15:57:34 +0400

    KS-1-5 flight time

--- src/main/kotlin/lesson_2/lesson2_task5_test1.kt
+++ src/main/kotlin/lesson_2/lesson2_task5_test1.kt
@@ -2,12 +2,12 @@ package org.example.lesson_2
 
 fun main() {
     val seconds = 6480
-    val a = 60
-    val minutes = seconds / a
-    val hour = minutes / a
-    val second = seconds % a
-    val minute = minutes % a
+    val numberOfSecond = 60
+    val minutes = seconds / numberOfSecond
+    val hour = minutes / numberOfSecond
+    val second = seconds % numberOfSecond
+    val minute = minutes % numberOfSecond
 
-    println("Время полета : ${"%02d".format(hour) + ":" + "%02d".format(minute) + ":" + "%02d".format(second)}")
+    println("Время полета : ${"%02d:%02d:%02d".format(hour, minute, second)}")
 
 }
