19c46a7bb133f96d4c9b40bc7da292d15555be4b
kirill <mrcreepipc@mail.ru>
2025-08-30 08:01:09 +0400

    KS-2-2 staff and print

--- src/main/kotlin/lesson_2/lesson2_task2.kt
+++ src/main/kotlin/lesson_2/lesson2_task2.kt
@@ -2,9 +2,11 @@ package org.example.lesson_2
 
 fun main() {
     val greetingForOrder = "Спасибо за ваш заказ!"
-    var numberOfOrders = 75
+    val numberOfOrders = 75
     println(greetingForOrder)
     println(numberOfOrders)
-    //val numberOfStaff = 2000
-    var numberOfStaff = 1999
+    var numberOfStaff = 2000
+    //println(numberOfStaff)
+    numberOfStaff = 1999
+    println(numberOfStaff)
 }
