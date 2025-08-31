cb564a8e3ecaad944f07fd4cc3bcf3279de4700c
kirill <mrcreepipc@mail.ru>
2025-08-31 13:35:49 +0400

    KS-2-2 employee payment

--- src/main/kotlin/lesson_2/lesson2_task2.kt
+++ src/main/kotlin/lesson_2/lesson2_task2.kt
@@ -1,13 +1,16 @@
 package org.example.lesson_2
 
 fun main() {
-    val greetingForOrder = "Спасибо за ваш заказ!"
-    val numberOfOrders = 75
-    println(greetingForOrder)
-    println(numberOfOrders)
-    var numberOfStaff = 2000
-    //println(numberOfStaff)
-    numberOfStaff = 1999
-    println(numberOfStaff)
+    val payEmployee = 30000
+    val payIntern = 20000
+    val numberOfEmployee = 50
+    val numberOfIntern = 30
 
+    val payCostEmployee = payEmployee * numberOfEmployee
+    val payCostFull = payEmployee * numberOfEmployee + payIntern * numberOfIntern
+    val averagePayCost = payCostFull/(numberOfEmployee+numberOfIntern)
+    println("Расход на зарплаты сотрудников: $payCostEmployee " +
+            "\nРасходы на зарплату сотрудников и стажеров: $payCostFull" +
+            "\nСредняя зарплата сотрудника: $averagePayCost ")
 }
+
