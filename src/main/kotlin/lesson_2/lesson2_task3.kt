6cbfbe2735199f352912d09e697baa189f09456a
kirill <mrcreepipc@mail.ru>
2025-08-31 19:04:23 +0400

    KS-2-3  travel time

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -1,14 +1,13 @@
 package org.example.lesson_2
-
+const val NUMBER_OF_MINUTE_IN_HOUR: Int = 60
 fun main() {
-    val numberOfMinuteInHour = 60
     val hourOfDeparture = 9
     val minuteOfDeparture = 39
-    val departureTime = hourOfDeparture * numberOfMinuteInHour + minuteOfDeparture
+    val departureTime = hourOfDeparture * NUMBER_OF_MINUTE_IN_HOUR + minuteOfDeparture
     val travelTime = 457
     val timeOfWay = departureTime + travelTime
-    val hourComing = timeOfWay / numberOfMinuteInHour
-    val minuteComing = timeOfWay % numberOfMinuteInHour
+    val hourComing = timeOfWay / NUMBER_OF_MINUTE_IN_HOUR
+    val minuteComing = timeOfWay % NUMBER_OF_MINUTE_IN_HOUR
     println("Время прибытия поезда: ${"%02d:%02d".format(hourComing, minuteComing)}")
 
 }
