65bdd1ddc2d13c33fed59666f81f4e80be17f99e
kirill <mrcreepipc@mail.ru>
2025-08-31 15:39:19 +0400

    KS-3-3  multiplication table

---
+++ src/main/kotlin/lesson_3/lesson3_task3.kt
@@ -0,0 +1,37 @@
+package org.example.lesson_3
+
+fun main() {
+    val inputNumber = 5
+    println("Вводимое число: $inputNumber")
+    val number1 = 1
+    val number2 = 2
+    val number3 = 3
+    val number4 = 4
+    val number5 = 5
+    val number6 = 6
+    val number7 = 7
+    val number8 = 8
+    val number9 = 9
+
+    val finishedNumber1 = inputNumber * number1
+    val finishedNumber2 = inputNumber * number2
+    val finishedNumber3 = inputNumber * number3
+    val finishedNumber4 = inputNumber * number4
+    val finishedNumber5 = inputNumber * number5
+    val finishedNumber6 = inputNumber * number6
+    val finishedNumber7 = inputNumber * number7
+    val finishedNumber8 = inputNumber * number8
+    val finishedNumber9 = inputNumber * number9
+
+    println(
+        "$inputNumber x $number1 = $finishedNumber1" +
+                "\n$inputNumber x $number2 = $finishedNumber2" +
+                "\n$inputNumber x $number3 = $finishedNumber3" +
+                "\n$inputNumber x $number4 = $finishedNumber4" +
+                "\n$inputNumber x $number5 = $finishedNumber5" +
+                "\n$inputNumber x $number6 = $finishedNumber6" +
+                "\n$inputNumber x $number7 = $finishedNumber7" +
+                "\n$inputNumber x $number8 = $finishedNumber8" +
+                "\n$inputNumber x $number9 = $finishedNumber9"
+    )
+}
