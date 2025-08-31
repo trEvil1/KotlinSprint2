63af7fce2c6c2ed86326f3e10eb6b0345f4355fc
kirill <mrcreepipc@mail.ru>
2025-08-31 14:04:00 +0400

    KS-2-1 average number

--- src/main/kotlin/lesson_2/lesson2_task1.kt
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -1,11 +1,12 @@
 package org.example.lesson_2
 
 fun main() {
-    val estimination_1 = 3.0
-    val estimination_2 = 4.0
-    val estimination_3 = 3.0
-    val estimination_4 = 5.0
-    var averageNumber = (estimination_4+estimination_3+estimination_2+estimination_1)/4
+    val mark1 = 3.0
+    val mark2 = 4.0
+    val mark3 = 3.0
+    val mark4 = 5.0
+    val numberOfStudents = 4
+    val averageNumber = (mark4 + mark3 + mark2 + mark1) / numberOfStudents
     println(averageNumber)
 
 }
