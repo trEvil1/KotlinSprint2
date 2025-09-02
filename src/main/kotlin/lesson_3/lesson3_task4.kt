57c5bfb5dcf5b258a95b71445c63554f4f7d6eb4
kirill <mrcreepipc@mail.ru>
2025-09-02 17:17:18 +0400

    KS-3-4

--- src/main/kotlin/lesson_3/lesson3_task4.kt
+++ src/main/kotlin/lesson_3/lesson3_task4.kt
@@ -4,13 +4,13 @@ fun main() {
     var turnFrom = "E2"
     var turnTo = "E4"
     var numberOfTurn = "1"
-    var turn = "$turnFrom $turnTo Номер хода: $numberOfTurn"
+    var turn = "$turnFrom-$turnTo:$numberOfTurn"
     println(turn)
 
     turnFrom = "D2"
     turnTo = "D3"
     numberOfTurn = "2"
-    turn = "$turnFrom $turnTo Номер хода: $numberOfTurn"
+    turn = "$turnFrom-$turnTo:$numberOfTurn"
 
     println(turn)
 }
