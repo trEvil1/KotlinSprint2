750bee1672ec7180aced578291dfd5dc7eb3e723
kirill <mrcreepipc@mail.ru>
2025-09-02 17:11:35 +0400

    KS-2-5

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -1,11 +1,12 @@
 package org.example.lesson_2
 
+import kotlin.math.pow
+
 fun main() {
     val clientDeposit = 70000
     val interestRate = 16.7
     val period = 20.0
-    val result = ("%.3f".format(clientDeposit * Math.pow(period, 1 + interestRate / 100)))
+    val result = ("%.3f".format(clientDeposit * (1 + interestRate / 100).pow(period)))
     println(result)
-
 
 }
