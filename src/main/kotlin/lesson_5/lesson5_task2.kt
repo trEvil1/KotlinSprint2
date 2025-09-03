d3f2692714bf3471e28ba2430c0c8c456bdf2a3f
kirill <mrcreepipc@mail.ru>
2025-09-03 17:55:55 +0400

    KS-5-2

---
+++ src/main/kotlin/lesson_5/lesson5_task2.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_5
+const val AGE_OF_MAJORITY = 18
+fun main() {
+    val currentYear = 2025
+    val enteredYear = 2000
+    val resultText = if (currentYear-enteredYear >= AGE_OF_MAJORITY){
+        "Show special content"
+    }else if(currentYear-enteredYear==16 || currentYear-enteredYear==17){
+        "Show limited content"
+    }else{
+        "Back to main screen"
+    }
+    println(resultText)
+}
