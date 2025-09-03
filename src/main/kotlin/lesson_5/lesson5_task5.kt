ca767aa16922deb82d5bacba2ac2f136b60883d3
kirill <mrcreepipc@mail.ru>
2025-09-03 20:44:07 +0400

    KS-5-5

--- src/main/kotlin/lesson_5/lesson5_task5.kt
+++ src/main/kotlin/lesson_5/lesson5_task5.kt
@@ -4,19 +4,19 @@ fun main() {
     val enteredNumber1 = readln().toInt()
     val enteredNumber2 = readln().toInt()
     val enteredNumber3 = readln().toInt()
-    val listOfEnteredNumbers = listOf(enteredNumber1,enteredNumber2,enteredNumber3)
+    val listOfEnteredNumbers = listOf(enteredNumber1, enteredNumber2, enteredNumber3)
     val winNumber1 = (0..42).random()
     val winNumber2 = (0..42).random()
     val winNumber3 = (0..42).random()
-    val listOfWinningNumber = listOf(winNumber1,winNumber2,winNumber3)
+    val listOfWinningNumber = listOf(winNumber1, winNumber2, winNumber3)
     val quantityOfNumbers = listOfWinningNumber.intersect(listOfEnteredNumbers).size
-    if (quantityOfNumbers == 3){
+    if (quantityOfNumbers == 3) {
         println("Поздравляю вы выйграли джекпот")
-    }else if (quantityOfNumbers == 2){
+    } else if (quantityOfNumbers == 2) {
         println("Поздравляю вы выйграли крупный выйгрыш")
-    }else if (quantityOfNumbers == 1){
+    } else if (quantityOfNumbers == 1) {
         println("Поздравляю вы выйграли утешительный приз")
-    }else{
+    } else {
         println("Вы не угадали ни одного числа")
     }
     println("Загаданные числа: $listOfWinningNumber")
