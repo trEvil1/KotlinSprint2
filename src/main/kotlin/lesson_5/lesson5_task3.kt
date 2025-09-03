c451459f247df7c9000bce297d1a1f34bb4a2e25
kirill <mrcreepipc@mail.ru>
2025-09-03 21:13:47 +0400

    KS-5-3

--- src/main/kotlin/lesson_5/lesson5_task3.kt
+++ src/main/kotlin/lesson_5/lesson5_task3.kt
@@ -3,14 +3,13 @@ package org.example.lesson_5
 const val WIN_NUMBER1 = 42
 const val WIN_NUMBER2 = 5
 fun main() {
-    val enteredNumber1 = 5
-    val enteredNumber2 = 42
-    if (enteredNumber2 > 42 || enteredNumber2 < 0 || enteredNumber1 > 42 || enteredNumber2 < 0) {
+    val enteredNumber1 = readln().toInt()
+    val enteredNumber2 = readln().toInt()
+    if (enteredNumber2 >= 42 || enteredNumber2 <= 0 || enteredNumber1 >= 42 || enteredNumber2 <= 0) {
         println("Одно из введенных чисел не верно")
     } else if (enteredNumber2 == WIN_NUMBER2 && enteredNumber1 == WIN_NUMBER1 || enteredNumber1 == WIN_NUMBER2 && enteredNumber2 == WIN_NUMBER1) {
         println("Поздравляю вы выйграли")
     } else {
         println("Не повезло")
     }
-
 }
