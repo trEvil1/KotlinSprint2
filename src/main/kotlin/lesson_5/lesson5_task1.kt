e4783ff63231d56d792ea325e60fd2cc66a6d9a4
kirill <mrcreepipc@mail.ru>
2025-09-03 20:58:27 +0400

    KS-5-1

--- src/main/kotlin/lesson_5/lesson5_task1.kt
+++ src/main/kotlin/lesson_5/lesson5_task1.kt
@@ -3,11 +3,11 @@ package org.example.lesson_5
 fun main() {
     val number1 = 5
     val number2 = 7
-    val amount = number2+number1
+    val amount = number2 + number1
     val enteredNumber = 5
-    if (enteredNumber == amount){
+    if (enteredNumber == amount) {
         println("Добро пожаловать")
-    }else{
+    } else {
         println("Доступ запрещен")
     }
 }
