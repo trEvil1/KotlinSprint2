d4d92ffe4047854649c271412593019fd9f3b053
kirill <mrcreepipc@mail.ru>
2025-09-11 16:31:19 +0400

    KS-7-3

--- src/main/kotlin/lesson7_task3.kt
+++ src/main/kotlin/lesson7_task3.kt
@@ -3,11 +3,9 @@ package org.example
 fun main() {
     println("Введите число:")
     val enteredNumber = readln().toInt()
-    for (i in 0..enteredNumber) {
-        if (i % 2 == 0) {
-            println(i)
-        } else {
-            continue
-        }
+    for (i in 0..enteredNumber step 2) {
+
+        println(i)
+
     }
 }
