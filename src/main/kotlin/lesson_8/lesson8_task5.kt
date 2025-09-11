6e9bb30db6be5c0a95901b6baaf36abb45d38e40
kirill <mrcreepipc@mail.ru>
2025-09-11 13:48:09 +0400

    KS-8-5

---
+++ src/main/kotlin/lesson_8/lesson8_task5.kt
@@ -0,0 +1,13 @@
+package org.example.lesson_8
+
+fun main() {
+    println("Сколько ингредиентов вы хотите добавить?")
+    val numberOfIngredients = readln().toInt()
+    var arrayOfIngredients = emptyArray<String>()
+    println("Введите $numberOfIngredients ингредиентов")
+    for (i in 0 until numberOfIngredients){
+        val addIngredient = readln()
+        arrayOfIngredients+= addIngredient
+    }
+    println(arrayOfIngredients.joinToString(","))
+}
