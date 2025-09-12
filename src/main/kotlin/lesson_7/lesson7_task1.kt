5f9f5835c3eb52d5cc27758a8bd910f8af884f8b
kirill <mrcreepipc@mail.ru>
2025-09-12 12:15:46 +0400

    KS-7-1

--- src/main/kotlin/lesson_7/lesson7_task1.kt
+++ src/main/kotlin/lesson_7/lesson7_task1.kt
@@ -4,13 +4,15 @@ const val ALL_CHAR = "qwertyuiopasdfghjklzxcvbnm"
 const val ALL_NUMBERS = "1234567890"
 
 fun main() {
-    val passwordLength = 3
+    val passwordLength = 6
     val password = mutableListOf<Char>()
     for (i in passwordLength downTo 1) {
-        val randomChar = ALL_CHAR.random()
-        val randomNumber = ALL_NUMBERS.random()
-        password.add(randomChar)
-        password.add(randomNumber)
+        if (i % 2 == 1) {
+            password.add(ALL_CHAR.random())
+        }
+        if (i % 2 == 0) {
+            password.add(ALL_NUMBERS.random())
+        }
     }
     println(password.joinToString(""))
 }
