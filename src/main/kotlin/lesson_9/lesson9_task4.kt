73ccc5acb261bf7507e06fbc6b6ec3ae208e79cb
kirill <mrcreepipc@mail.ru>
2025-09-12 14:16:14 +0400

    KS-9-4

---
+++ src/main/kotlin/lesson_9/lesson9_task4.kt
@@ -0,0 +1,7 @@
+package org.example.lesson_9
+
+fun main() {
+    println("Введите 5 ингредиентов")
+    val listOfIngredients = readln().split(",")
+    println(listOfIngredients.toList().sorted())
+}
