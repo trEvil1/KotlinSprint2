d3c72adb9b77ab49d94793c1857b06d97bdca3ca
kirill <mrcreepipc@mail.ru>
2025-09-12 12:11:07 +0400

    KS-7-4

--- src/main/kotlin/lesson_7/lesson7_task4.kt
+++ src/main/kotlin/lesson_7/lesson7_task4.kt
@@ -2,9 +2,9 @@ package org.example.lesson_7
 
 fun main() {
     println("Сколько секунд засечь?")
-    var seconds = readln().toInt()
-    for (i in seconds downTo 0) {
-        println("Осталось ${seconds--} секунд")
+    val seconds = readln().toInt()
+    for (i in seconds downTo 1) {
+        println("Осталось $i секунд")
         Thread.sleep(1000)
     }
     println("Время вышло")
