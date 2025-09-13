3507c1b3a63d0deddcf51d73ceed4c995642c064
kirill <mrcreepipc@mail.ru>
2025-09-13 22:37:19 +0400

    KS-9-3

--- src/main/kotlin/lesson_9/lesson9_task3.kt
+++ src/main/kotlin/lesson_9/lesson9_task3.kt
@@ -3,12 +3,15 @@ package org.example.lesson_9
 fun main() {
     val listOfIngredients = listOf("яйцо", "молоко", "сливочное масло")
     val listOfAmountIngredients = listOf(2, 50, 15)
-    println("Сколко порций нужно?")
+    println("Сколько порций нужно?")
     val amountOfPortions = readln().toInt()
     println(
-        "На $amountOfPortions порций вам понадобится:" +
-                "${listOfIngredients[0]}-${listOfAmountIngredients[0] * amountOfPortions} " +
-                "${listOfIngredients[1]}-${listOfAmountIngredients[1] * amountOfPortions} " +
-                "${listOfIngredients[2]}-${listOfAmountIngredients[2] * amountOfPortions} "
+        "На $amountOfPortions поций вам понадобится " +
+                listOfAmountIngredients.map {
+                    (it * amountOfPortions).toString() + "-" + listOfIngredients.get(
+                        listOfAmountIngredients.indexOf(it)
+                    )
+                }.joinToString(", ")
     )
 }
+
