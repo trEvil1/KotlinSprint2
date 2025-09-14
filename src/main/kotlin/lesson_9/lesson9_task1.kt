30a7e53c89f699edf7684173a374928d0b6f2264
kirill <mrcreepipc@mail.ru>
2025-09-14 08:38:17 +0400

    KS-9-1

--- src/main/kotlin/lesson_9/lesson9_task1.kt
+++ src/main/kotlin/lesson_9/lesson9_task1.kt
@@ -3,5 +3,5 @@ package org.example.lesson_9
 fun main() {
     val ingredients = listOf("капуста", "салат", "морковь", "томат")
     println("В рецепте есть следущиюе ингредиенты: ")
-    ingredients.forEach{ println(it) }
+    ingredients.forEach { println(it) }
 }
