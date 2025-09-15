7060894db44694020b1df3069c78f1b2fa48be58
kirill <mrcreepipc@mail.ru>
2025-09-15 19:41:17 +0400

    KS-10-2

--- src/main/kotlin/lesson_10/lesson10_task2.kt
+++ src/main/kotlin/lesson_10/lesson10_task2.kt
@@ -2,15 +2,18 @@ package org.example.lesson_10
 
 fun main() {
     println("Введите логин")
-    val login = readln()
+    val login = checkLength(readln())
     println("Введите пароль")
-    val password = readln()
-    if (checkLength(login) < 4 || checkLength(password) < 4) {
-        println("Логин или пароль недостаточно длинные")
-    } else println("Добро пожаловать")
+    val password = checkLength(readln())
+    if (login == true && password == true) {
+        println("Добро пожаловать")
+    } else println("Логин или пароль недостаточно длинные")
 }
 
-fun checkLength(length: String): Int {
-    val checkLength = length.length
-    return checkLength
+fun checkLength(length: String): Boolean {
+    if (length.length < 4) {
+        return false
+    } else {
+        return true
+    }
 }
