a5f43f326bcd1c20a757bdd691eb1d6ae192c42d
kirill <mrcreepipc@mail.ru>
2025-09-16 20:18:14 +0400

    KS-11-4

---
+++ src/main/kotlin/lesson_11/lesson11_task4.kt
@@ -0,0 +1,12 @@
+package org.example.lesson_11
+
+class RecipeCategory(recipes: MutableList<Recipe>, name: String ) {
+
+}
+
+class Recipe(val name:String, ingredients: MutableList<Ingredient>, val cookWay:String) {
+
+}
+
+class Ingredient(val name: String, val count: Int, val countType: String)
+
