224fa0dfd404ec4424ceb197c9f811a6258ceec6
kirill <mrcreepipc@mail.ru>
2025-09-16 17:42:49 +0400

    KS-11-1

--- src/main/kotlin/lesson_11/task1/lesson11_task1_class.kt
+++
@@ -1,5 +0,0 @@
-package org.example.lesson_11.task1
-
-class User (val login:String, val password:String, val email:String,){
-
-}
--- src/main/kotlin/lesson_11/task1/lesson11_task1_main.kt
+++ src/main/kotlin/lesson_11/task1/lesson11_task1_main.kt
@@ -1,10 +1,13 @@
 package org.example.lesson_11.task1
 
 fun main() {
-    val user1: User = User("admin", "12345", "admin@gmail.com")
-    val user2: User = User("admin1", "123456", "admin1@gmail.com")
+    val user1 = User("admin", "12345", "admin@gmail.com")
+    val user2 = User("admin1", "123456", "admin1@gmail.com")
     println(
         "${user1.login}, ${user1.password}, ${user1.email} " +
                 ": ${user2.login}, ${user2.password}, ${user2.email}"
     )
 }
+
+class User(val login: String, val password: String, val email: String)
+
