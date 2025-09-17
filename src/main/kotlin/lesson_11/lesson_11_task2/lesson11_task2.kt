2ed765434c5ee324fa18d854d5101ecdf4222d9e
kirill <mrcreepipc@mail.ru>
2025-09-17 17:21:44 +0400

    KS-11-2

--- src/main/kotlin/lesson_11/lesson_11_task2/lesson11_task2.kt
+++ src/main/kotlin/lesson_11/lesson_11_task2/lesson11_task2.kt
@@ -2,13 +2,13 @@ package org.example.lesson_11.lesson_11_task2
 
 
 fun main() {
-    val userInfo: User2 = User2("admin1", "pass", "lol@gmail.com")
+    val userInfo: User2 = User2("4325612","admin1", "pass", "lol@gmail.com")
     userInfo.passwordChange()
     userInfo.printUserInfo()
 
 }
 
-class User2(val login: String, var password: String, val email: String, var bio: String? = null) {
+class User2(val id: String, val login: String, var password: String, val email: String, var bio: String? = null) {
 
     fun passwordChange() {
         println("Введите пароль")
@@ -26,7 +26,7 @@ class User2(val login: String, var password: String, val email: String, var bio:
     }
 
     fun printUserInfo() {
-        println("$login $password $email $bio")
+        println("$login $password $email $bio $id")
     }
 
     fun setBio() {
