55644f50cf29bf4f273263849a7969f83de80b20
kirill <mrcreepipc@mail.ru>
2025-09-19 20:57:43 +0400

    KS-11-5

--- src/main/kotlin/lesson_11/lesson11_task5.kt
+++ src/main/kotlin/lesson_11/lesson11_task5.kt
@@ -1,39 +1,68 @@
 package org.example.lesson_11
 
 fun main() {
-    val forum = Forum(User(), User(), User(), 0)
-    forum.createNewUser("user1")
-    forum.createNewUser("user2")
+    val forum = Forum()
+    forum.createNewUser("Питек")
+    forum.createNewUser("Димуля")
     forum.createNewMessage(1)
     forum.createNewMessage(2)
-    forum.printThread(forum.users.names, forum.userMessage.message)
+    forum.createNewMessage(1)
+    forum.createNewMessage(2)
+    forum.createNewMessage(1)
+    forum.createNewMessage(2)
+
+    forum.printThread()
 }
+class Forum(){
+    val users = mutableMapOf<Int, User>()
+    val messages = mutableMapOf<Int, Message>()
+    var authorId = 1
+    var messagesId = 1
 
-class Forum(var userId: User, var users: User, var userMessage: User, var index: Int) {
-    fun createNewUser(user: String): String {
-        index += 1
-        userId.ID = index
-        users.names.put(userId.ID, user)
-        return user
+    fun createNewUser(userName: String){
+        val user = User(userName, authorId)
+        users[authorId] = user
+        authorId+=1
     }
 
-    fun createNewMessage(id: Int) {
-        if (id in users.names.keys) {
-            println("Введите сообщение")
-            val newMessage = readln()
-            userMessage.message.put(id, newMessage)
-        } else return
+    fun createNewMessage(id: Int){
+        if (id in users.keys){
+            println("Введите сообщение: ")
+            val message = readln()
+            messages[messagesId] = Message(message, id)
+            messagesId+=1
+        }
     }
 
-    fun printThread(user: Map<Int, String>, userMessage: Map<Int, String>) {
-        for (i in user.keys) {
-            println("${user.get(i)}: ${userMessage.get(i)}")
+    fun printThread(){
+        for (i in messages){
+            println("${users[i.value.userId]?.name}:${i.value.text}")
         }
     }
 }
 
-class User() {
-    var names = mutableMapOf<Int, String>()
-    var ID = 1
-    var message = mutableMapOf<Int, String>()
-}
+
+class User(val name: String, val id: Int)
+
+class Message(var text: String, var userId: Int)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
