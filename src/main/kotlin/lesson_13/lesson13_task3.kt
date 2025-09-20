f8f319eaec4e385aa9eea9e1b821e255c4520f0b
kirill <mrcreepipc@mail.ru>
2025-09-20 14:37:32 +0400

    KS-13-3

---
+++ src/main/kotlin/lesson_13/lesson13_task3.kt
@@ -0,0 +1,16 @@
+package org.example.lesson_13
+
+fun main() {
+    val user1 = Directory2("user1", 89123456789)
+    val user2 = Directory2("user2", 89123456789)
+    val user3 = Directory2("user3", 89123456789, "null")
+    val user4 = Directory2("user4", 89123456789, "Lol")
+    val user5 = Directory2("user5", 89123456789, "OOO")
+    val userList = mutableListOf(user5, user4, user3, user2, user1)
+    mutableListOf(userList.map { if (it.company != null) println(it.company) })
+
+}
+
+class Directory2(val name: String, val phoneNumber: Long, var company: String? = null)
+
+
