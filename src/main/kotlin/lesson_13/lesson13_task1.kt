4892285f0dedb19a1deb105b23d632a46819caad
kirill <mrcreepipc@mail.ru>
2025-09-20 14:14:56 +0400

    KS-13-1

---
+++ src/main/kotlin/lesson_13/lesson13_task1.kt
@@ -0,0 +1,7 @@
+package org.example.lesson_13
+
+fun main() {
+    val directory = Directory("user1", 89063429435,)
+}
+
+class Directory (val name:String, val phoneNumber:Long, val company:String? = null,)
