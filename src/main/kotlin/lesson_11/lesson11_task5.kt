72aaf30c65322a03d485fdbbf2420602756afa11
kirill <mrcreepipc@mail.ru>
2025-09-21 16:40:59 +0400

    KS-11-5

--- src/main/kotlin/lesson_11/lesson11_task5.kt
+++ src/main/kotlin/lesson_11/lesson11_task5.kt
@@ -4,65 +4,44 @@ fun main() {
     val forum = Forum()
     forum.createNewUser("Питек")
     forum.createNewUser("Димуля")
-    forum.createNewMessage(1)
-    forum.createNewMessage(2)
-    forum.createNewMessage(1)
-    forum.createNewMessage(2)
-    forum.createNewMessage(1)
-    forum.createNewMessage(2)
+    forum.createNewMessage(1," 434")
+    forum.createNewMessage(2,"ghfhfgh")
+    forum.createNewMessage(1,"5456")
+    forum.createNewMessage(2,"gfdgdf")
+    forum.createNewMessage(1," 54354353")
+    forum.createNewMessage(2,"dfdfg")
 
     forum.printThread()
 }
-class Forum(){
+
+class Forum {
     val users = mutableMapOf<Int, User>()
     val messages = mutableMapOf<Int, Message>()
     var authorId = 1
     var messagesId = 1
 
-    fun createNewUser(userName: String){
+    fun createNewUser(userName: String):User {
         val user = User(userName, authorId)
         users[authorId] = user
-        authorId+=1
+        authorId += 1
+        return user
     }
 
-    fun createNewMessage(id: Int){
-        if (id in users.keys){
-            println("Введите сообщение: ")
-            val message = readln()
+    fun createNewMessage(id: Int, message: String) {
+        if (id in users.keys) {
             messages[messagesId] = Message(message, id)
-            messagesId+=1
+            messagesId += 1
         }
     }
 
-    fun printThread(){
-        for (i in messages){
+    fun printThread() {
+        for (i in messages) {
             println("${users[i.value.userId]?.name}:${i.value.text}")
         }
     }
 }
 
-
 class User(val name: String, val id: Int)
 
 class Message(var text: String, var userId: Int)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
