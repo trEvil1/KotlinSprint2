287d103257fd3e294e1f4742dd984bb063fc6738
kirill <mrcreepipc@mail.ru>
2025-09-22 09:39:26 +0400

    KS-16-1

---
+++ src/main/kotlin/lesson_16/lesson16_task1.kt
@@ -0,0 +1,11 @@
+package org.example.lesson_16
+
+fun main() {
+    val dice = Dice()
+    dice.print()
+}
+class Dice(val randomNumber:Int = (1..6).random()){
+    fun print(){
+        println(randomNumber)
+    }
+}
