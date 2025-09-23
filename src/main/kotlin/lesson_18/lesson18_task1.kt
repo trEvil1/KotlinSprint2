9475994c76029d65be595509e97db6c74e26098d
kirill <mrcreepipc@mail.ru>
2025-09-23 19:50:06 +0400

    KS-18-1

--- src/main/kotlin/lesson_18/lesson18_task1.kt
+++ src/main/kotlin/lesson_18/lesson18_task1.kt
@@ -2,15 +2,16 @@ package org.example.lesson_18
 
 fun main() {
     val order = Order()
-    order.getOrder("веревка",1)
-    order.getOrder(listOf("лопата", "савок"),2)
+    order.getOrder("веревка", 1)
+    order.getOrder(listOf("лопата", "савок"), 2)
 }
-class Order(){
-    fun getOrder(order: String, orderNumber:Int){
-        println("Номер заказа $orderNumber заказ: $order")
-    }
-    fun getOrder(order: List<String>, orderNumber:Int){
+
+class Order() {
+    fun getOrder(order: String, orderNumber: Int) {
         println("Номер заказа $orderNumber заказ: $order")
     }
 
+    fun getOrder(order: List<String>, orderNumber: Int) {
+        println("Номер заказа $orderNumber заказ: $order")
+    }
 }
