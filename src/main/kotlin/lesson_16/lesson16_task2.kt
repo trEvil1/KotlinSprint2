46e43aa5ac94703df057de64752ed9f0a2001b0d
kirill <mrcreepipc@mail.ru>
2025-09-23 13:37:41 +0400

    KS-16-2

--- src/main/kotlin/lesson_16/lesson16_task2.kt
+++ src/main/kotlin/lesson_16/lesson16_task2.kt
@@ -1,7 +1,9 @@
 package org.example.lesson_16
 
+import kotlin.math.pow
+
 fun main() {
-    val circle = Circle(5)
+    val circle = Circle(5.0)
     circle.area()
     println("Периметр %.2f".format(circle.perimeter()))
     println("Площадь %.2f".format(circle.area()))
@@ -9,15 +11,10 @@ fun main() {
 
 private const val PI = 3.14
 
-class Circle(private val radius: Int) {
+class Circle(private val radius: Double) {
 
-    fun perimeter(): Double {
-        val perimeter = 2 * PI * radius
-        return perimeter
-    }
+    fun perimeter(): Double = 2 * PI * radius
 
-    fun area(): Double {
-        val area = PI * Math.pow(radius.toDouble(), 2.0)
-        return area
-    }
+    fun area(): Double = PI * radius.pow(2)
+
 }
