35a6b3b20c03d7a2565f4fa94eabafa6275719f8
kirill <mrcreepipc@mail.ru>
2025-09-23 15:29:40 +0400

    KS-17-4

---
+++ src/main/kotlin/lesson_17/lesson17_4.kt
@@ -0,0 +1,20 @@
+package org.example.lesson_17
+
+fun main() {
+    val pack = Package(1, "Москва")
+    pack.newPlace = "Самара"
+    pack.newPlace = "Волгоград"
+    println("${pack.index} ${pack.packageNumber} ${pack.newPlace}")
+}
+
+class Package(private val number: Int, private var place: String, var index: Int = 0) {
+    var packageNumber = number
+    var newPlace = place
+        get() = field
+        set(value: String) {
+            field = value
+            if (field != place) {
+                index += 1
+            }
+        }
+}
