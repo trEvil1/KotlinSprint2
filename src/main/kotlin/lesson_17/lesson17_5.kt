bbd6b036a30c02d04667c7c7b933c52e83c67f85
kirill <mrcreepipc@mail.ru>
2025-09-27 17:49:17 +0400

    KS-17-5

--- src/main/kotlin/lesson_17/lesson17_5.kt
+++ src/main/kotlin/lesson_17/lesson17_5.kt
@@ -2,26 +2,24 @@ package org.example.lesson_17
 
 fun main() {
     val user = User("login1", "password1")
-    println(user.newPassword)
-    user.newLogin = "login1111"
-    user.newPassword = "dgdg"
-    println(user.newPassword)
+    println(user.password)
+    user.login = "login1111"
+    user.password = "dgdg"
+    println(user.password)
 }
 
-class User(private var login: String, password: String) {
-    var newPassword = password
-        get() = field.replace(Regex("(?=.)."), "*")
+class User(login: String, password: String) {
+    var password = password
+        get() = "*".repeat(field.length)
         set(value) {
             println("Вы не можете изменить пароль")
-            field = value
         }
-    var newLogin = login
-        get() = field
+    var login = login
         set(value) {
             field = value
             if (value != login) {
                 login = value
-                println("Логин успешно изменен на $login")
+                println("Логин успешно изменен на $value")
             } else println("У вас такой же логин")
         }
 }
