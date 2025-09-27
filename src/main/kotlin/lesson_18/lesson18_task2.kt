bfd34cf56211dfddaf42d15a9971cc7bd98d31b6
kirill <mrcreepipc@mail.ru>
2025-09-27 19:45:29 +0400

    KS-18-2

--- src/main/kotlin/lesson_18/lesson18_task2.kt
+++ src/main/kotlin/lesson_18/lesson18_task2.kt
@@ -1,32 +1,37 @@
 package org.example.lesson_18
 
 fun main() {
-    val dice4 = Dice4((1..4).random())
-    val dice6 = Dice6((1..6).random())
-    val dice8 = Dice8((1..8).random())
+    val dice4 = Dice4()
+    val dice6 = Dice6()
+    val dice8 = Dice8()
     val diceList = listOf(dice4, dice6, dice8)
     diceList.forEach { it.throwDice() }
 }
 
-abstract class Throw {
+abstract class Dice(var dice: Int) {
     open fun throwDice() {
-    }
-}
-
-class Dice4(val dice: Int) : Throw() {
-    override fun throwDice() {
+        dice = (1..dice).random()
         println(dice)
     }
 }
 
-class Dice6(val dice: Int) : Throw() {
+class Dice4 : Dice(4) {
     override fun throwDice() {
+        dice = (1..dice).random()
         println(dice)
     }
 }
 
-class Dice8(var dice: Int) : Throw() {
+class Dice6 : Dice(6) {
     override fun throwDice() {
+        dice = (1..dice).random()
+        println(dice)
+    }
+}
+
+class Dice8 : Dice(8) {
+    override fun throwDice() {
+        dice = (1..dice).random()
         println(dice)
     }
 }
