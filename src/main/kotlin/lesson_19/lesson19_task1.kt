21a9fdf909e0aa5a79117df6c89a968a3ca00c3e
kirill <mrcreepipc@mail.ru>
2025-09-28 18:01:52 +0400

    KS-19-1

--- src/main/kotlin/lesson_19/lesson19_task1.kt
+++ src/main/kotlin/lesson_19/lesson19_task1.kt
@@ -2,10 +2,10 @@ package org.example.lesson_19
 
 fun main() {
     println(
-        "Вы можете добавить в аквариум:${Fishes.GUPPY}," +
-                " ${Fishes.GOLDFISH}, " +
-                "${Fishes.ANGELFISH}, " +
-                "${Fishes.SIAMESE_FIGHTING_FISH}"
+        "Вы можете добавить в аквариум:${Fishes.GUPPY.fish}," +
+                " ${Fishes.GOLDFISH.fish}, " +
+                "${Fishes.ANGELFISH.fish}, " +
+                Fishes.SIAMESE_FIGHTING_FISH.fish
     )
 }
 
