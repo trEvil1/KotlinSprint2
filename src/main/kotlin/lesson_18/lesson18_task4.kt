1e03965f8c984b1c70b2c95ace3a0424d256c2d8
kirill <mrcreepipc@mail.ru>
2025-09-28 08:21:44 +0400

    KS-18-4

--- src/main/kotlin/lesson_18/lesson18_task4.kt
+++ src/main/kotlin/lesson_18/lesson18_task4.kt
@@ -5,24 +5,23 @@ import kotlin.math.pow
 fun main() {
     val pack1 = CubePack(5.0)
     val pack2 = RectanglePack(4.0, 5.0, 7.0)
-    println(listOf(pack2, pack1).forEach { it.getArea() })
+    listOf(pack2, pack1).forEach { println(it.getArea()) }
 }
 
-open class AreaPack {
-    open fun getArea() {
-    }
+abstract class AreaPack {
+    abstract fun getArea(): Double
 }
 
 class CubePack(private val length: Double) : AreaPack() {
-    override fun getArea() {
-        val area = (6 * length).pow(2)
-        println("Посылка куб ее площадь $area")
+    override fun getArea(): Double {
+        val area = 6 * length.pow(2)
+        return area
     }
 }
 
 class RectanglePack(private val length: Double, private val width: Double, private val height: Double) : AreaPack() {
-    override fun getArea() {
-        val area = length * width * height
-        println("Посылка прямоугольник ее площадь $area")
+    override fun getArea(): Double {
+        val area = 2 * (length * width + length * height + height * width)
+        return area
     }
 }
