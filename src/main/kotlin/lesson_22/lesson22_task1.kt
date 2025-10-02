e9e1b433235046e3813d2b1e2aad65dd3028c8e3
kirill <mrcreepipc@mail.ru>
2025-10-02 07:19:31 +0400

    KS-22-1

--- src/main/kotlin/lesson_22/lesson22_task1.kt
+++ src/main/kotlin/lesson_22/lesson22_task1.kt
@@ -5,7 +5,6 @@ fun main() {
     val dataBook = DataBook("123", "user")
     println(regularBook.name == dataBook.name)
     println(regularBook.author == dataBook.author)//тут мы сравнимаем свойства и указываем их и они идентичны
-    println(regularBook == dataBook)// тут сравниваются строки вывода самих объектов и у regularBook это непонятная строка а у DataBook это понятный строковый вывод объектов
 }
 
 class RegularBook(val name: String, val author: String)
